05b23ab984c9f9bb3be8b00f1b3bbb1c415ddf26
Vicky Gray <vicky.gray@digital.justice.gov.uk>
2016-12-29 12:18:17 +0000

    Completed Hello World and Baby steps

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,1 +1,7 @@
-console.log(process.argv);
+var args = process.argv.splice(1 + 1);
+var numbers = args.map(Number);
+var sum = numbers.reduce(function(a, b) {
+  return a + b;
+}, 0);
+
+console.log(sum);
